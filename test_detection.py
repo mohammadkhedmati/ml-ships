d325ec99eb477ec41241ddb098bdd7857002c405
mohammad khedmati <mohammad.khedmati2012@gmail.com>
2024-03-03 10:10:03 +0330

    run y9

--- test_detection.py
+++ test_detection.py
@@ -4,11 +4,12 @@ from matplotlib import pyplot as plt
 import numpy as np
 from ensemble_boxes import *
 import ultralytics
+from ultralytics.utils.benchmarks import benchmark
 
 
 image_path = 'Object_detection\\total_ds_test_imgs_yolo\\0-19824.JPEG'
 
-model1 = YOLO("runs\\detect\\ship_detection_yoloV8m3\\weights\\last.pt")
+model1 = YOLO("yolov9\\runs\\train\\yolov9-c-ships3\\weights\\best.pt")
 model2 = YOLO("runs\\detect\\ship_detection_yoloV8n\\weights\\best.pt")
 
 # results1 = model1.predict(image_path, save_txt=True)
@@ -24,15 +25,12 @@ if __name__ == '__main__':
     metrics1.box.map75,  # map75
     metrics1.box.maps   # a list contains map50-95 of each category
     )
-    metrics2 = model2.val()  # no arguments needed, dataset and settings remembered
-    print(metrics2.box.map,    # map50-95
-    metrics2.box.map50,  # map50
-    metrics2.box.map75,  # map75
-    metrics2.box.maps   # a list contains map50-95 of each category
-    )
+    # metrics2 = model2.val()  # no arguments needed, dataset and settings remembered
+    # print(metrics2.box.map,    # map50-95
+    # metrics2.box.map50,  # map50
+    # metrics2.box.map75,  # map75
+    # metrics2.box.maps   # a list contains map50-95 of each category
+    # )
 
-
-# from ultralytics.utils.benchmarks import benchmark
-
-# # Benchmark on GPU
-# benchmark(model='runs\\detect\\ship_detection_yoloV8n\\weights\\best.pt', data='datatest.yaml', imgsz=640, half=False, device=0)
+    # Benchmark on GPU
+    # benchmark(model='runs\\detect\\ship_detection_yoloV8n\\weights\\best.pt', data='data.yaml',  half=False, device=0)
--- yolov9/run scripts.sh
+++ yolov9/run scripts.sh
@@ -1,7 +1,7 @@
 python train_dual.py --device 0 --batch 16 --epochs 100 --data Object_detection/data.yaml --img 640 --cfg models/detect/yolov9-c.yaml --weights yolov9-c.pt --name yolov9-c-custom --hyp hyp.scratch-high.yaml --min-items 0 --epochs 500 --close-mosaic 15
 
 # train yolov9 models
-python train_dual.py --workers 8 --device 0 --batch 8 --data Object_detection/data.yaml --img 640 --cfg models/detect/yolov9-c.yaml --weights yolov9-c.pt --name yolov9-c-ships --hyp hyp.scratch-high.yaml --min-items 0 --epochs 100 --close-mosaic 15
+python train_dual.py --workers 8 --device 0 --batch 8 --data Object_detection/data.yaml --img 640 --cfg models/detect/yolov9-c.yaml --weights yolov9-c.pt --name yolov9-c-ships --hyp hyp.scratch-high.yaml --min-items 0 --epochs 3 --close-mosaic 15
 
 # train gelan models
 # python train.py --workers 8 --device 0 --batch 32 --data data/coco.yaml --img 640 --cfg models/detect/gelan-c.yaml --weights '' --name gelan-c --hyp hyp.scratch-high.yaml --min-items 0 --epochs 500 --close-mosaic 15
